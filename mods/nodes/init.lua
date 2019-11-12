25c79435f70cb31cb178b216c9ecc0fa2924f04a
LoneWolfHT <lonewolf04361@gmail.com>
2019-11-11 16:46:09 -0800

    Add forge node. Set up LBM to set meta after placing schem

--- mods/nodes/init.lua
+++ mods/nodes/init.lua
@@ -1,5 +1,17 @@
 nodes = {}
 
+minetest.register_lbm({ -- Loads nodes placed by schematics that have meta
+	label = "Load special nodes placed by schematics",
+	name = "nodes:loadspecialnodes",
+	nodenames = {"group:loadme"},
+	run_at_every_load = true,
+	action = function(pos, node)
+		if minetest.get_meta(pos):get_int("loaded") ~= 0 then return end -- Already loaded
+
+		minetest.registered_nodes[node.name].on_construct(pos)
+	end
+})
+
 --
 --- Include the rest of the mod's lua files
 --
