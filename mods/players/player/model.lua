eeb15c6fc4d1f4ac0db4e92ad9228c743c08f0d4
LoneWolfHT <lonewolf04361@gmail.com>
2020-06-22 21:53:41 -0700

    Update player model again, still WIP

--- mods/players/player/model.lua
+++ mods/players/player/model.lua
@@ -1,10 +1,14 @@
 local anims = {
-	stand = {x = 0, y = 0},
-	sit = {x = 1, y = 1},
-	walk = {x = 2, y = 42},
-	mine = {x = 43, y = 57},
-	lay = {x = 58, y = 58},
-	walk_mine = {x = 59, y = 103},
+	stand = {range = {x = 0, y = 0}},
+	sit = {range = {x = 1, y = 1}},
+	lay = {range = {x = 2, y = 2}},
+	walk = {range = {x = 3, y = 27}},
+	walk_mine = {range = {x = 28, y = 52}},
+	mine = {range = {x = 53, y = 77}},
+	swim_mine = {range = {x = 78, y = 108}, speed = 28},
+	swim_up = {range = {x = 109, y = 133}, speed = 28},
+	swim_down = {range = {x = 134, y = 158}, speed = 28},
+	wave = {range = {x = 159, y = 171}, speed = 34}
 }
 
 minetest.register_on_joinplayer(function(player)
@@ -15,7 +19,7 @@ minetest.register_on_joinplayer(function(player)
 		textures = {"player.png"},
 	})
 
-	player:set_local_animation(anims.stand, anims.walk, anims.mine, anims.walk_mine, 60)
+	player:set_local_animation(anims.stand.range, anims.walk.range, anims.mine.range, anims.walk_mine.range, 40)
 end)
 
 minetest.register_globalstep(function()
@@ -26,14 +30,14 @@ minetest.register_globalstep(function()
 
 		if controls.right or controls.left or controls.down or controls.up then
 			if controls.lmb or controls.rmb then
-				player:set_animation(anims.walk_mine, 60)
+				player:set_animation(anims.walk_mine.range, anims.walk_mine.speed)
 			else
-				player:set_animation(anims.walk, 60)
+				player:set_animation(anims.walk.range, anims.walk.speed)
 			end
 		elseif controls.lmb or controls.rmb then
-			player:set_animation(anims.mine, 60)
+			player:set_animation(anims.mine.range, anims.mine.speed)
 		else
-			player:set_animation(anims.stand, 60)
+			player:set_animation(anims.stand.range, anims.stand.speed)
 		end
 	end
 end)
