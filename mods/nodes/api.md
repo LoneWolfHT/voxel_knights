12ab6213f3d6bc8a00b41e86b40ad2773e4901d4
LoneWolfHT <lonewolf04361@gmail.com>
2020-08-14 14:05:55 -0700

    Add register_on_nodeload()

---
+++ mods/nodes/api.md
@@ -0,0 +1,7 @@
+# Node mod API
+
+```lua
+nodes.register_on_nodeload(function(pos, node)
+ -- Do stuff to a schematic node after it's been constructed for the first time
+end)
+```
--- mods/nodes/init.lua
+++ mods/nodes/init.lua
@@ -1,4 +1,10 @@
-nodes = {}
+nodes = {
+	registered_on_loadnode = {}
+}
+
+function nodes.register_on_nodeload(func)
+	table.insert(nodes.registered_on_loadnode, func)
+end
 
 minetest.register_lbm({ -- Loads nodes placed by schematics that have meta
 	label = "Load special nodes placed by schematics",
@@ -9,6 +15,12 @@ minetest.register_lbm({ -- Loads nodes placed by schematics that have meta
 		if minetest.get_meta(pos):get_int("loaded") ~= 0 then return end -- Already loaded
 
 		minetest.registered_nodes[node.name].on_construct(pos)
+
+		for _, func in ipairs(nodes.registered_on_loadnode) do
+			if func(pos, node) then
+				break
+			end
+		end
 	end
 })
 
